a2f0b40965414bf5b78f9f578484c3f07cb60a9e
janitha-visna <visnajanitha@gmail.com>
2024-04-21 10:24:23 +0530

    add react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,13 @@ export default function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p class="font-light ...">The quick brown fox ...</p>
+      <p class="font-semibold ...">The quick brown fox ...</p>
+      <p class="font-light ...">The quick brown fox ...</p>
+      <p class="font-normal ...">The quick brown fox ...</p>
+      <p class="font-medium ...">The quick brown fox ...</p>
+      <p class="font-semibold ...">The quick brown fox ...</p>
+      <p class="font-bold ...">The quick brown fox ...</p>
     </div>
   );
 }
