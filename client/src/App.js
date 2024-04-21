45f3aa2a17aba7eb970c7833330f058d674fedfc
janitha-visna <visnajanitha@gmail.com>
2024-04-21 10:54:34 +0530

    create routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
 import React from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+
+/**import all components */
+import Username from './components/Username';
+import Password from './components/Password';
+import Register from './components/Register';
+import Profile from "./components/Profile";
+import Recovery from "./components/Recover";
+import Reset from "./components/Reset";
+import PageNotFound from "./components/PageNotFound";
+
+
+/** root routes */
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p class="font-light ...">The quick brown fox ...</p>
-      <p class="font-semibold ...">The quick brown fox ...</p>
-      <p class="font-light ...">The quick brown fox ...</p>
-      <p class="font-normal ...">The quick brown fox ...</p>
-      <p class="font-medium ...">The quick brown fox ...</p>
-      <p class="font-semibold ...">The quick brown fox ...</p>
-      <p class="font-bold ...">The quick brown fox ...</p>
-    </div>
+    <main>
+        <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
