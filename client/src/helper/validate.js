22170624109e6b2310585cb168c3e5381fa4b809
janitha-visna <visnajanitha@gmail.com>
2024-04-22 09:53:40 +0530

    password validation

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -1,12 +1,35 @@
 import toast from "react-hot-toast";
 
 /**validate login page username */
-export async function usernameValidate(values){
-    const errors = usernameVerify ({},values);
-    
-    return errors;
+export async function usernameValidate(values) {
+  const errors = usernameVerify({}, values);
+
+  return errors;
 }
 
+/**validate password */
+export async function passwordValidate(values){
+    const errors = passwordVerify({},values);
+return errors;
+
+}
+
+
+/**validate password */
+function passwordVerify(errors = {}, values) {
+  const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+
+  if (!values.password) {
+    errors.password = toast.error("password required..!");
+  } else if (values.password.includes(" ")) {
+    errors.password = toast.error("wrong password !!");
+  } else if (values.password.length < 4) {
+    errors.password = toast.error("password must be more that 4 charters");
+  } else if (!specialChars.test(values.password)) {
+    errors.password = toast.error("password must have specila charaters");
+  }
+  return errors;
+}
 
 /** valitde username */
 
