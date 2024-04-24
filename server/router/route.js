d33e932d41def0c11e967da9ae0ebb390efaf634
janitha-visna <visnajanitha@gmail.com>
2024-04-24 10:12:55 +0530

    endpoints created

---
+++ server/router/route.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+const router = Router(); // Create a new instance of the Router class
+
+/**import all contrllers */
+import * as controller from '../controller/appController.js'
+
+/**POST method */
+router.route("/register").post(controller.register);
+
+router.route("/authenticate").post((req,res) => res.end()); //authenticate user
+router.route("/login").post(controller.login) //login in app
+
+/**GET method */
+
+router.route("user/:username").get(controller.getUser);
+router.route("/generateOTP").get(controller.generateOTP);
+router.route("/verifyOTP").get(controller.verifyOTP);
+router.route("/createResetSession").get(controller.createResetSession);
+
+/**PUT method */
+router.route("/updateuser").put(controller.updateuser);
+router.route("/resetPassword").put(controller.resetPassword);
+
+export default router;
