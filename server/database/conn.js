d4b94fd45a3a753bdb690f98beee74ff426bca5e
janitha-visna <visnajanitha@gmail.com>
2024-04-24 09:29:45 +0530

    mongodb connection initiated

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,1 +1,14 @@
 import mongoose  from "mongoose";
+import {MongoMemoryServer} from "mongodb-memory-server";
+
+async function connect(){
+     const mongod = await MongoMemoryServer.create();
+     const getUri = mongod.getUri();
+
+     mongoose.set('strictQuery',true)
+     const db = await mongoose.connect(getUri);
+     console.log("databse connected")
+     return db;
+}
+
+export default connect;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connect from "./database/conn.js";
 
 const app = express();
 
@@ -8,18 +9,26 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
-app.disable('x-powered-by'); // less hackers know about our stack
+app.use(morgan("tiny"));
+app.disable("x-powered-by"); // less hackers know about our stack
 
 const port = 8080;
 
 /**HTTP GET request */
 
-app.get('/',(req,res) => {
-    res.status(201).json("home GET request");
+app.get("/", (req, res) => {
+  res.status(201).json("home GET request");
 });
 
-/**start server */
-app.listen(port,() => {
-     console.log(`server connected to  http://localhost:${port}`);
+/**start server only when we have valid connection */
+connect().then(() => {
+  try {
+    app.listen(port, () => {
+      console.log(`server connected to  http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("cannot connect to the server");
+  }
+}).catch(error => {
+    console.log("invalid datbase connection..!");
 })
