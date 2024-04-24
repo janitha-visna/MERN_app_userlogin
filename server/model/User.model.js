1a1dbb4090f6eae92f576445e1ac080f3a7db5ab
janitha-visna <visnajanitha@gmail.com>
2024-04-24 13:42:18 +0530

    mongodb schema created

---
+++ server/model/User.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+export const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "please provide unique Username"],
+    unique: [true, "username Exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "please provide a password"],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, "please provide a unique email"],
+    unique: true,
+  },
+  firstName: { type: String },
+  lastName: { type: String },
+  mobile: { type: String },
+  profile: { type: String },
+});
+
+export default mongoose.model.Users || mongoose.model("User", UserSchema);
