0d742dbe1d4e8dd6751fada1028066128d6d3882
janitha-visna <visnajanitha@gmail.com>
2024-04-28 13:17:27 +0530

    make api requests

---
+++ client/src/helper/helper.js
@@ -0,0 +1,117 @@
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+
+/**make api request */
+
+/**authenticate funtion */
+
+export async function authenticate(username) {
+  try {
+    return await axios.post("/api/authenticate", { username });
+  } catch (error) {
+    return { error: "username doesnt't exist " };
+  }
+}
+
+/**get user details */
+
+export async function getUser({ username }) {
+  try {
+    const { data } = await axios.get(`/api/user/${username}`);
+    return { data };
+  } catch (error) {
+    return { error: "password doesn't match " };
+  }
+}
+
+/**register user funtion */
+export async function registerUser(credentials) {
+  try {
+    const {
+      data: { msg },
+      status,
+    } = await axios.post(`/api/register`, credentials);
+    let { username, email } = credentials;
+
+    /**send email  */
+    if (status === 201) {
+      await axios.post("/api/registerMail", {
+        username,
+        userEmail: email,
+        text: msg,
+      });
+    }
+
+    return Promise.resolve(msg);
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+/**login funtion */
+export async function verifyPassword({ username, password }) {
+  try {
+    const { data } = await axios.post("/api/login", { username, password });
+    return Promise.resolve({ data });
+  } catch (error) {
+    return Promise.reject({ error: "password doesnt match" });
+  }
+}
+
+/**update user funtion */
+export async function updateUser(response) {
+  try {
+    const token = await localStorage.getItem("token");
+    const data = await axios.put("/api/updateuser", response, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    return Promise.resolve({ data });
+  } catch (error) {
+    return Promise.reject({ error: "couldn't update profile" });
+  }
+}
+
+/**generate OTP */
+
+export async function generateOTP(username) {
+  try {
+    const {
+      data: { code },
+      status,
+    } = await axios.get("/api/generateOTP", { params: { username } });
+
+    //send mail with OTP
+    if (status === 201) {
+      let {
+        data: { email },
+      } = await getUser({ username });
+      let text = `your password recovery otp  is ${code}.verify and recover your password`;
+      await axios.post('/api/registerMail',{username,userEmail:email,text,subject : "password recover otp"})
+    }
+    return Promise.resolve(code)
+  } catch (error) {
+    return Promise.reject({ error });
+  }
+}
+
+/** verify otp */
+export async function verifyOTP({username,code}){
+    try {
+       const {data,status} = await axios.get('/api/verifyOTP',{params : {username,code}})
+       return {data,status}
+    } catch (error) {
+        return Promise.reject(error);
+    }
+}
+
+/**reset password */
+export async function resetPassword({username,password}){
+    try {
+        const {data,status} = await axios.put('/api/resetPassword',{username,password});
+        return Promise.resolve({data,status})
+    } catch (error) {
+        return Promise.reject({error})
+    }
+}
