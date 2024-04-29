4de9a43e1b915a0367ac9c0f9d5f722d10795002
janitha-visna <visnajanitha@gmail.com>
2024-04-29 11:49:23 +0530

    store the token in localstorage and navigate to profile component

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import avatar from "../assets/avatar.png";
 import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import toast , { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { passwordValidate } from "../helper/validate";
 import useFetch from "../hooks/fetch.hook";
 import { useAuthStore } from "../store/store";
+import { verifyPassword } from "../helper/helper";
 
 export default function Password() {
+
+  const navigate = useNavigate()
   const { username } = useAuthStore((state) => state.auth);
   const [{ isLoading, apiData, serverError }] = useFetch(`/user/${username}`);
 
@@ -21,10 +24,23 @@ export default function Password() {
     validateOnChange: false,
     onSubmit: async (values) => {
       console.log(values);
+
+      let loginPromise = verifyPassword({username,password:values.password})
+      toast.promise(loginPromise,{
+        loading:'checking..',
+        success:<b>login successfuly</b>,
+        error:<b>password not match</b>
+      });
+
+      loginPromise.then(res => {
+        let {token} = res.data;
+        localStorage.setItem('token',token)
+        navigate('/profile')
+      })
     },
   });
 
-  if(isLoading) return <h1 className='text-2xl font-bold'>is loading</h1>;
+  //if(isLoading) return <h1 className='text-2xl font-bold'>is loading</h1>;
   if(serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>
 
   return (
