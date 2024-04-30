bc0533d5ac9132314053c86fcf5b4c6e1d641a5a
janitha-visna <visnajanitha@gmail.com>
2024-04-30 09:58:24 +0530

    password recovery

--- client/src/components/Recover.js
+++ client/src/components/Recover.js
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import avatar from "../assets/avatar.png";
 import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { passwordValidate } from "../helper/validate";
+import { useAuthStore } from "../store/store";
+import { generateOTP, verifyOTP } from "../helper/helper";
+import { useNavigate } from "react-router-dom";
 
 export default function Recovery() {
-    
+  const { username } = useAuthStore((state) => state.auth);
+  const { OTP, setOTP } = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    generateOTP(username).then((OTP) => {
+      console.log(OTP);
+      if (OTP) return toast.success("OTP has been send to your email");
+      return toast.error("problem while generating OTP");
+    });
+  }, [username]);
+
+  async function onSubmit(e) {
+    e.preventDefault();
+
+    try {
+      let { status } = await verifyOTP({ username, code: OTP });
+      if (status == 201) {
+        toast.success("verify successfuly");
+        return navigate("/reset");
+      }
+    } catch (error) {
+      return toast.error("wrong otp!check mail again");
+    }
+  }
+
+  //handler of resend OTP
+  function resendOTP() {
+    let sendPromise = generateOTP(username);
+
+    toast.promise(sendPromise, {
+      loading: "sending..",
+      success: <b>otp has been send to your email</b>,
+      error: <b>could not send it</b>,
+    });
+
+    sendPromise.then((OTP) => {
+      console.log(OTP);
+    });
+  }
+
   return (
     <div className="container mx-auto">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
@@ -19,17 +62,21 @@ export default function Recovery() {
               enter OTP to recover password
             </span>
           </div>
-          <form className="pt-20">
+          <form className="pt-20" onSubmit={onSubmit}>
             <div className="textbox flex flex-col items-center gap-6">
               <div className="input text-center ">
-                <input type="text" placeholder="OTP" />
+                <input
+                  onChange={(e) => setOTP(e.target.value)}
+                  type="text"
+                  placeholder="OTP"
+                />
               </div>
               <span className="py-4 text-sm text-left text-gray-500">
                 enter 6 digit OTP sent to your email adress
               </span>
 
               <button className={styles.btn} type="submit">
-                sign in{" "}
+                recover
               </button>
             </div>
             <div className="text-center py-4">
